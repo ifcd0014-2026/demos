c6364f0df80bea027de1f33f058567cb9a8527c0
JD-CFTIC <jd.cftic@gmail.com>
2026-09-23 12:06:59 +0200

    S02 demo-01: estructura proyecto Maven

---
+++ s02/demo-01-estructura/src/test/java/com/curso/AppTest.java
@@ -0,0 +1,12 @@
+package com.curso;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void pruebaBasica() {
+        assertTrue(true);
+    }
+}
